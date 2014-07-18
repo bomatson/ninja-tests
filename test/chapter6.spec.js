3fdad80b5796524f0ecf825c0741acf9b4e28826
bomatson <bobby@carbonfive.com>
2014-07-18 09:25:29 -0700

    learn about object tpyes and reconciling references

--- test/chapter6.spec.js
+++ test/chapter6.spec.js
@@ -3,24 +3,24 @@ var expect = require('chai').expect;
 describe('Chapter 6', function() {
   describe('Instantiation', function() {
     context('with a constructor', function() {
-      var giles;
+      var reference;
 
       beforeEach(function() {
-        function Drummer() { };
-        Drummer.prototype.openStroke = function() { return 'open stroke!' };
-        giles = new Drummer();
+        function Creation() { };
+        Creation.prototype.create = function() { return 'create' };
+        reference = new Creation();
       });
 
       it('creates a newly allocated object', function() {
-        expect(giles).to.be.defined;
+        expect(reference).to.be.defined;
       });
 
       it('attaches the prototype methods to the reference', function() {
-        expect(giles.openStroke).to.be.defined
+        expect(reference.create).to.not.be.undefined
       });
 
       it('allows prototype methods to be called', function() {
-        expect(giles.openStroke()).to.eq('open stroke!');
+        expect(reference.create()).to.eq('create');
       });
     });
 
@@ -50,9 +50,63 @@ describe('Chapter 6', function() {
           };
         };
         Drummer.prototype.openStroke = function() { return 'open stroke!'; };
-        bobby = new Drummer();
+        var bobby = new Drummer();
 
         expect(bobby.openStroke()).to.eq('open stroke as instance method!');
+      });
+    });
+
+    context('reference reconciliation', function() {
+      it('live updates the object prototype once referenced', function() {
+        function Drummer() { };
+        var bobby = new Drummer();
+
+        Drummer.prototype.openStroke = function() { return 'open stroke!'; };
+        expect(bobby.openStroke()).to.eq('open stroke!');
+      });
+
+      it('cannot update the prototype if the object has already been referenced', function() {
+        function Drummer() { };
+        var bobby = new Drummer();
+
+        expect(bobby.doubleStroke).to.be.undefined
+        Drummer.prototype.doubleStroke = function() { return 'double stroke'; };
+      });
+
+      it('still resolves property references first', function() {
+        function Drummer() {
+          this.openStroke = function() {
+            return 'open stroke as instance method!';
+          };
+        };
+        var bobby = new Drummer();
+        Drummer.prototype.openStroke = function() { return 'open stroke!'; };
+
+        expect(bobby.openStroke()).to.eq('open stroke as instance method!');
+      });
+    });
+
+    context('object typing', function() {
+      it('uses instanceOf to find the constructor', function() {
+        function Creation() { };
+        reference = new Creation();
+
+        expect(reference instanceof Creation).to.eq(true);
+      });
+
+      it('objects have a constructor property', function() {
+        function Creation() { };
+        reference = new Creation();
+
+        expect(reference.constructor).to.eq(Creation);
+      });
+
+      it('constructor properties can build objects even when no longer in scope', function() {
+        function Creation() { };
+        reference = new Creation();
+        another_reference = new reference.constructor();
+
+        expect(another_reference instanceof Creation).to.eq(true);
       });
     });
   });
